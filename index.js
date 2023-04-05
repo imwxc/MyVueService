db175749d286cd6aca4f2d7d5a51094b433d8b79
imwxc <1362451456@qq.com>
2023-04-05 20:08:56 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/person', (req,res)=>{
+    res.send({
+        name: 'tom',
+        age: 18
+    })
+})
+
+app.listen(5005, (err)=>{
+    if(!err) console.log('服务器启动成功')
+})
